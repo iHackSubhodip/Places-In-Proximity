a69ce3ecaf19f5146ab0ef5cbaf13f38107fd033
Banerjee, Subhodip <Subhodip.Banerjee@Honeywell.com>
2018-08-27 00:05:51 +0530

    FLow layout added.

--- PlacesInProximity/PlacesInProximity/Controller/Places/PlacesCollectionViewController.swift
+++ PlacesInProximity/PlacesInProximity/Controller/Places/PlacesCollectionViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreLocation
+import AVFoundation
 
 private let reuseIdentifier = "PlacesCell"
 class PlacesCollectionViewController: UICollectionViewController {
@@ -36,27 +37,6 @@ class PlacesCollectionViewController: UICollectionViewController {
         collectionView?.contentInset = UIEdgeInsets.init(top: 0, left: 15.0, bottom: 0, right: 15.0)
     }
     
-    // MARK: UICollectionViewDataSource
-
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        performSegue(withIdentifier: AppConstants.SegueIdentifier.mapSegueIdentifier, sender: indexPath)
-    }
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return places.count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? PlacesCollectionViewCell else {
-            fatalError("could not dequeue cell")
-        }
-        let place = places[indexPath.row]
-        cell.update(place: place)
-    
-        return cell
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == AppConstants.SegueIdentifier.mapSegueIdentifier && sender is IndexPath {
             if let vc = segue.destination as? PlaceMapsViewController {
@@ -66,4 +46,89 @@ class PlacesCollectionViewController: UICollectionViewController {
             }
         }
     }
+}
+
+// MARK: 'UICollectionViewDelegate'
+extension PlacesCollectionViewController{
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: AppConstants.SegueIdentifier.mapSegueIdentifier, sender: indexPath)
+    }
+}
+
+
+// MARK: 'UICollectionViewDataSource'
+extension PlacesCollectionViewController{
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return places.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //Make it generic
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? PlacesCollectionViewCell else {
+            fatalError("could not dequeue cell")
+        }
+        let place = places[indexPath.row]
+        cell.update(place: place)
+        
+        return cell
+    }
+    
+}
+
+// MARK: 'UICollectionViewDelegateFlowLayout'
+extension PlacesCollectionViewController{
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 15.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let cellPadding: CGFloat = 20.0
+        let columnWidth:CGFloat = 250
+        let imageWidth = columnWidth
+        let labelWidth = columnWidth - cellPadding * 2
+        
+        let photoHeight = heightForPhotoAtIndexPath(indexPath: indexPath, withWidth: imageWidth)
+        let annotationHeight = heightForAnnotationAtIndexPath(indexPath: indexPath, withWidth: labelWidth)
+        let height = photoHeight + annotationHeight
+        
+        return CGSize.init(width: columnWidth, height: height)
+    }
+    
+    // MARK: Calculates the height of photo
+    func heightForPhotoAtIndexPath(indexPath: IndexPath,
+                                   withWidth width: CGFloat) -> CGFloat {
+        
+        var size = CGSize.init(width: CGFloat(MAXFLOAT), height: 1)
+        let place = places[indexPath.row]
+        
+        guard let photo = place.photos?.first, place.photos?.first?.photoRefKey != nil, let widthP = photo.width, let heightP = photo.height else {
+            return 0
+        }
+        
+        size = CGSize.init(width: CGFloat(widthP), height: CGFloat(heightP))
+        
+        let boundingRect =  CGRect(x: 0, y: 0, width: width, height: CGFloat(MAXFLOAT))
+        let rect  = AVMakeRect(aspectRatio: size, insideRect: boundingRect)
+        
+        return rect.size.height
+    }
+    
+    // MARK: Calculates the height label
+    func heightForAnnotationAtIndexPath(indexPath: IndexPath, withWidth width: CGFloat) -> CGFloat {
+        
+        let place = places[indexPath.row]
+        let annotationPadding = CGFloat(5)
+        
+        let font = UIFont.systemFont(ofSize: 15)
+        let commentHeight = place.heightForComment(font, width: width)
+        
+        let height = annotationPadding + commentHeight + annotationPadding
+        
+        return height
+    }
+    
 }
